7cca5dbba6c27ecf6a3f4c23a244f9efed7c2a30
suyu <13936562@qq.com>
2020-10-30 15:06:33 +0800

    部署gh_pages分支

--- scripts/build.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack')
-const webpackConfig = require('./webpack.config')
-const logger = console
-
-webpack(webpackConfig, (err, stats) => {
-  logger.info('Build complete.')
-})
